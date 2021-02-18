04a3b322e82f7c3bd03844256d057aecabcbfe48
imanid-code <imanidillahunt@gmail.com>
2021-02-18 16:25:14 -0500

    adding comments to understand provided deve code

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,9 +1,11 @@
+//Declare variable without value so they value can be changed throughout the code 
 let noteTitle;
 let noteText;
 let saveNoteBtn;
 let newNoteBtn;
 let noteList;
 
+//if the pathname is /notes , select the following classes and assign them new variable names 
 if (window.location.pathname === '/notes') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
@@ -25,6 +27,8 @@ const hide = (elem) => {
 // activeNote is used to keep track of the note in the textarea
 let activeNote = {};
 
+//this will be a function in server.js that getNotes() which will equal gotNotesthis gotdata from getnotes and return got data as a string 
+//the server will get the input information 
 const getNotes = () =>
   fetch('/api/notes', {
     method: 'GET',
@@ -33,6 +37,13 @@ const getNotes = () =>
     },
   });
 
+  //in server.js postedNote will equal the req.body 
+  //gotNotes will = getNotes()
+  //then gotNotes/getNotes will push ... the rest of postedNote, 
+  //
+  //postedNote will = req.body 
+  //NOTE will be put into the body?
+  //what is taken from the body will be put into a string ?
 const saveNote = (note) =>
   fetch('/api/notes', {
     method: 'POST',
@@ -42,6 +53,8 @@ const saveNote = (note) =>
     body: JSON.stringify(note),
   });
 
+
+  //
 const deleteNote = (id) =>
   fetch(`/api/notes/${id}`, {
     method: 'DELETE',
@@ -52,7 +65,7 @@ const deleteNote = (id) =>
 
 const renderActiveNote = () => {
   hide(saveNoteBtn);
-
+//where is id coming from? 
   if (activeNote.id) {
     noteTitle.setAttribute('readonly', true);
     noteText.setAttribute('readonly', true);
@@ -63,12 +76,15 @@ const renderActiveNote = () => {
     noteText.value = '';
   }
 };
-
+//handleNoteSave is going to do a function called newNote which will have a title = notetitle & text = notetext 
 const handleNoteSave = () => {
   const newNote = {
     title: noteTitle.value,
     text: noteText.value,
   };
+  //SaveNote as defined on line 49 will POST to api/notes usign JSON to stringigy it to the page
+  //then getandrender notes will get notes from database and render(show)them on sidebar
+  //if activenote.id set notevariables 
   saveNote(newNote).then(() => {
     getAndRenderNotes();
     renderActiveNote();
@@ -76,10 +92,16 @@ const handleNoteSave = () => {
 };
 
 // Delete the clicked note
+//e stands for event 
 const handleNoteDelete = (e) => {
   // prevents the click listener for the list from being called when the button inside of it is clicked
+ //Propagation means bubbling up to parent elements or capturing down to child elements.
+
   e.stopPropagation();
 
+  //returns the element that triggers specific event 
+  //?????
+  //the specific event is note? 
   const note = e.target;
   const noteId = JSON.parse(note.parentElement.getAttribute('data-note')).id;
 
@@ -87,6 +109,7 @@ const handleNoteDelete = (e) => {
     activeNote = {};
   }
 
+  //delete the note id then put in side bar and render(show) active notes
   deleteNote(noteId).then(() => {
     getAndRenderNotes();
     renderActiveNote();
@@ -94,6 +117,7 @@ const handleNoteDelete = (e) => {
 };
 
 // Sets the activeNote and displays it
+//?
 const handleNoteView = (e) => {
   e.preventDefault();
   activeNote = JSON.parse(e.target.parentElement.getAttribute('data-note'));
@@ -105,7 +129,7 @@ const handleNewNoteView = (e) => {
   activeNote = {};
   renderActiveNote();
 };
-
+//if the title and text have a value , trim it and hide save button if they don't have value show save button
 const handleRenderSaveBtn = () => {
   if (!noteTitle.value.trim() || !noteText.value.trim()) {
     hide(saveNoteBtn);
@@ -114,6 +138,7 @@ const handleRenderSaveBtn = () => {
   }
 };
 
+//????
 // Render the list of note titles
 const renderNoteList = async (notes) => {
   let jsonNotes = await notes.json();
@@ -168,7 +193,8 @@ const renderNoteList = async (notes) => {
 };
 
 // Gets notes from the db and renders them to the sidebar
-const getAndRenderNotes = () => getNotes().then(renderNoteList);
+const getAndRenderNotes = 
+() => getNotes().then(renderNoteList);
 
 if (window.location.pathname === '/notes') {
   saveNoteBtn.addEventListener('click', handleNoteSave);
